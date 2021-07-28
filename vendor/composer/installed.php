8b290cfa33e4c6da8eb3c35c9bdca9dc4c7e9e92
Laura Lospitao <lauralospitao@gmail.com>
2021-07-28 11:38:10 +0200

    Register feature > User registration data validation #1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '94b0ce69c2418383d500c596a20a0f6b9d55bff9',
+    'name' => 'lauralospitao/parking-lot-manager',
+  ),
+  'versions' => 
+  array (
+    'lauralospitao/parking-lot-manager' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '94b0ce69c2418383d500c596a20a0f6b9d55bff9',
+    ),
+  ),
+);
